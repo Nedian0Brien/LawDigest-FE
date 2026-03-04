e6fdd337580c8aa828d1fe0fb6e054f9d70fba67
Minjae Park <parkmj9260@gmail.com>
2026-03-04 20:14:42 +0900

    CHORE: 테스트 도메인을 test.lawdigest.cloud에서 test.lawdigest.net으로 변경

--- next.config.js
+++ next.config.js
@@ -12,6 +12,23 @@ const nextConfig = {
     ],
     domains: [process.env.NEXT_PUBLIC_HOSTNAME],
   },
+  experimental: {
+    serverActions: {
+      allowedOrigins: ['test.lawdigest.net', 'https://test.lawdigest.net'],
+    },
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/v1/:path*',
+        destination: 'https://api.lawdigest.net/v1/:path*',
+      },
+      {
+        source: '/oauth2/:path*',
+        destination: 'https://api.lawdigest.net/oauth2/:path*',
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
